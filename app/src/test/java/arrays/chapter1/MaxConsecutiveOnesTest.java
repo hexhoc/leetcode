a32d7be542a3e5afaf7ee20e5883a6d27824d741
hexhoc <hexhoc@gmail.com>
2022-01-11 21:39:40 +0300

    Task 1

---
+++ app/src/test/java/arrays/chapter1/MaxConsecutiveOnesTest.java
@@ -0,0 +1,21 @@
+package arrays.chapter1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MaxConsecutiveOnesTest {
+    
+    @Test
+    @DisplayName("find max consecutive ones")
+    public void findMaxConsecutiveOnes() {
+        MaxConsecutiveOnes testClass = new MaxConsecutiveOnes();
+        assertEquals(3, testClass.findMaxConsecutiveOnes(new int[]{0,1,1,0,1,1,1}));
+        assertEquals(1, testClass.findMaxConsecutiveOnes(new int[]{0,1,0,1}));
+        assertEquals(0, testClass.findMaxConsecutiveOnes(new int[]{0,0,0,0,0,0,0}));
+        assertEquals(7, testClass.findMaxConsecutiveOnes(new int[]{1,1,1,1,1,1,1}));
+        assertEquals(1, testClass.findMaxConsecutiveOnes(new int[]{0,0,0,0,0,0,1}));
+        assertEquals(1, testClass.findMaxConsecutiveOnes(new int[]{1,0,0,0,0,0,0}));
+    }
+}
