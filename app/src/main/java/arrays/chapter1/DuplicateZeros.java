520f819182844653525e404dabb9da3efedf4b3d
hexhoc <hexhoc@gmail.com>
2022-01-12 10:28:59 +0300

    Task 5

--- app/src/main/java/arrays/chapter1/DuplicateZeros.java
+++ app/src/main/java/arrays/chapter1/DuplicateZeros.java
@@ -3,20 +3,28 @@ package arrays.chapter1;
 public class DuplicateZeros {
     public static void main(String[] args) {
         DuplicateZeros task = new DuplicateZeros();
-        task.execute(new int[]{1,0,2,3,0,4,5,0});
+        int[] arr = new int[]{1,0,2,3,0,4,5,0};
+        task.execute(arr);
         int a = 0;
     }
 
     public void execute(int[] arr) {
+
         int[] newArr = new int[arr.length];
 
         for (int i = 0, j = 0; j < arr.length; i++, j++) {
             newArr[j] = arr[i];
+
             if (arr[i] == 0) {
-                newArr[++j] = arr[i];
+                j++;
+                if (j != arr.length) {
+                    newArr[j] = arr[i];
+                }
             }
         }
 
-        arr = newArr;
+        for (int i = 0; i < newArr.length; i++) {
+            arr[i] = newArr[i];
+        }
     }
 }
