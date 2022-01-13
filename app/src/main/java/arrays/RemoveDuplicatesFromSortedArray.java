7dc2225663501e6314d0c6fd70a1492fcc584ac9
Журавский Владислав Валерьевич <v.zhuravskiy@rendez-vous.ru>
2022-01-13 11:08:43 +0300

    Task 7. Edit algorithm

--- app/src/main/java/arrays/RemoveDuplicatesFromSortedArray.java
+++ app/src/main/java/arrays/RemoveDuplicatesFromSortedArray.java
@@ -2,28 +2,35 @@ package arrays;
 
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
-        int[] arr = new int[]{0,0,1,1,1,2,2,3,3,4};
-        RemoveDuplicatesFromSortedArray.execute(arr);
+        int[] arr1 = new int[]{0,0,1,1,1,2,2,3,3,4};
+        RemoveDuplicatesFromSortedArray.execute(arr1);
+
+        int[] arr2 = new int[]{1,1,2};
+        RemoveDuplicatesFromSortedArray.execute(arr2);
+
+        int[] arr3 = new int[]{1};
+        RemoveDuplicatesFromSortedArray.execute(arr3);
     }
 
     public static int execute(int[] nums) {
-        int[] newNums = new int[nums.length];
-        int total = 0;
-        
-        for (int i = 0, k = 0; i < nums.length; i++) {
-            if (i == 0) {
-                newNums[k] = nums[i];
-                k++;
-                continue;
-            }
 
+        if (nums.length == 0) { 
+            return 0;
+        }
+
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i-1]) {
-                newNums[k] = nums[i];
-                k++;
-                total++;
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        return total;
+        //set empty values
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        return j;
     }
 }
--- app/src/test/java/arrays/RemoveDuplicatesFromSortedArrayTest.java
+++ app/src/test/java/arrays/RemoveDuplicatesFromSortedArrayTest.java
@@ -1,11 +1,24 @@
 package arrays;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class RemoveDuplicatesFromSortedArrayTest {
     
     @Test
     public void check() {
-        RemoveDuplicatesFromSortedArray.execute(new int[]{});
+        int[] arr1 = new int[]{0,0,1,1,1,2,2,3,3,4};
+        RemoveDuplicatesFromSortedArray.execute(arr1);
+
+        int[] arr2 = new int[]{1,1,2};
+        RemoveDuplicatesFromSortedArray.execute(arr2);
+
+        int[] arr3 = new int[]{1};
+        RemoveDuplicatesFromSortedArray.execute(arr3);
+
+        assertArrayEquals(new int[]{0,1,2,3,4,0,0,0,0,0}, arr1);
+        assertArrayEquals(new int[]{1,2,0}, arr2);
+        assertArrayEquals(new int[]{1}, arr3);
     }
 }
