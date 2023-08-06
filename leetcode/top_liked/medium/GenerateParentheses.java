3e835c02f0d59d48faaa8420a5d3e4c5de7fd8b1
hexhoc <hexhoc@gmail.com>
2023-08-06 22:43:39 +0300

    22. Generate Parentheses

---
+++ leetcode/top_liked/medium/GenerateParentheses.java
@@ -0,0 +1,49 @@
+package top_liked.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 22. Generate Parentheses
+ * TODO: Повторить, сложно.
+ * for input n = 2
+ * 								   	(0, 0, '')
+ * 								 	    |
+ * 									(1, 0, '(')
+ * 								   /           \
+ * 							(2, 0, '((')      (1, 1, '()')
+ * 							   /                 \
+ * 						(2, 1, '(()')           (2, 1, '()(')
+ * 						   /                       \
+ * 					(2, 2, '(())')                (2, 2, '()()')
+ * 						      |	                             |
+ * 					res.append('(())')             res.append('()()')
+ */
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+        var s = new GenerateParentheses();
+        System.out.println(s.generateParenthesis(5));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        recurse(result, "", 0, 0, n);
+        return result;
+    }
+
+    public void recurse(List<String> result, String s, int left, int right, int n) {
+        if (s.length() == n*2) {
+            result.add(s);
+            return;
+        }
+
+        if (left < n) {
+            recurse(result, s+"(", left+1, right, n);
+        }
+
+        if (right < left) {
+            recurse(result, s+")", left, right+1, n);
+        }
+    }
+}
