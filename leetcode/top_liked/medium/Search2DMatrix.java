67a6c41ea1c729397f048b8943a1885c618612c6
hexhoc <hexhoc@gmail.com>
2023-08-07 15:38:37 +0300

    74. Search a 2D Matrix (new solution)

--- leetcode/top_liked/medium/Search2DMatrix.java
+++ leetcode/top_liked/medium/Search2DMatrix.java
@@ -10,18 +10,37 @@ public class Search2DMatrix {
 
     public static void main(String[] args) {
         var s = new Search2DMatrix();
-        var matrix = new int[][]{{1}};
-        System.out.println(s.searchMatrix(matrix, 2));
+//        var matrix = new int[][]{{1}};
+//        System.out.println(s.searchMatrix(matrix, 2));
+//
+//        matrix = new int[][]{{1},{3}};
+//        System.out.println(s.searchMatrix(matrix, 3));
 
-        matrix = new int[][]{{1},{3}};
-        System.out.println(s.searchMatrix(matrix, 3));
-
-        matrix = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        var matrix = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
         System.out.println(s.searchMatrix(matrix, 3));
         System.out.println(s.searchMatrix(matrix, 13));
     }
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+
+    public boolean bad_searchMatrix(int[][] matrix, int target) {
         boolean result = false;
 //        int row = approximateBinarySearch(matrix, target);
         int row = approximateBinarySearch(matrix, target);
