d6dab87e18d1935b4456869c8e8dbf43544819e5
hexhoc <hexhoc@gmail.com>
2023-08-17 22:16:16 +0300

    26. Remove Duplicates from Sorted Array

--- leetcode/top_interview_150/array_string/RemoveDuplicatesFromSortedArray.java
+++ leetcode/top_interview_150/array_string/RemoveDuplicatesFromSortedArray.java
@@ -1,34 +1,8 @@
 package top_interview_150.array_string;
 
 /*
-Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that 
-each unique element appears only once. The relative order of the elements should be kept the same.
-
-Since it is impossible to change the length of the array in some languages, you must instead have 
-the result be placed in the first part of the array nums. More formally, if there are k elements 
-after removing the duplicates, then the first k elements of nums should hold the final result. 
-It does not matter what you leave beyond the first k elements.
-
-Return k after placing the final result in the first k slots of nums.
-
-Do not allocate extra space for another array. You must do this by modifying the input array 
-in-place with O(1) extra memory.
-
-
-Example 1:
-
-Input: nums = [1,1,2]
-Output: 2, nums = [1,2,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-
-Example 2:
-
-Input: nums = [0,0,1,1,1,2,2,3,3,4]
-Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-
+26. Remove Duplicates from Sorted Array
+Easy
 */
 
 import java.util.Arrays;
@@ -36,40 +10,33 @@ import java.util.Arrays;
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] arr1 = new int[]{0,0,1,1,1,2,2,3,3,4};
-        RemoveDuplicatesFromSortedArray.execute(arr1);
-
         int[] arr2 = new int[]{1,1,2};
-        RemoveDuplicatesFromSortedArray.execute(arr2);
-
         int[] arr3 = new int[]{1};
-        RemoveDuplicatesFromSortedArray.execute(arr3);
-
         int[] arr4 = new int[]{0,1,2,2,3,3,4,4,4};
-        RemoveDuplicatesFromSortedArray.execute(arr4);
 
+        var s = new RemoveDuplicatesFromSortedArray();
+        s.removeDuplicates(arr1);
+        s.removeDuplicates(arr2);
+        s.removeDuplicates(arr3);
+        s.removeDuplicates(arr4);
     }
 
-    public static int execute(int[] nums) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[i]!=nums[j]) {
-                i++;
-                nums[i] = nums[j];
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                continue;
             }
+
+            nums[k] = nums[i];
+            k++;
         }
 
-        for (int j = i; j < nums.length; j++) {
-            nums[j] = 0;
+        for (int i = k; i < nums.length; i++) {
+            nums[i] = 0;
         }
 
-        return i+1;
+        return k;
     }
 
-    public static int executeUsingStream(int[] nums) {
-        var numsDistinct = Arrays.stream(nums).boxed().distinct().toList();
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = i >= numsDistinct.size() ? 0 : numsDistinct.get(i);
-        }
-        return numsDistinct.size();
-    }
 }
