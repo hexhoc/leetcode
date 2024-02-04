2190627a8627e91f078a4fdabff8da36c1163895
ZHURAVSKY Vladislav V <Vladislav.ZHURAVSKY@raiffeisen.ru>
2024-02-04 19:17:46 +0300

    update tests

--- go-contest/contest_2023_1/b_problem/Main.java
+++ go-contest/contest_2023_1/b_problem/Main.java
@@ -1,33 +1,53 @@
-
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder result = new StringBuilder();
-        int size = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < size; i++) {
-            String str = sc.nextLine();
-            result.append(isShipCorrect(str) + "\r\n");
+    public static void main(String[] args) throws IOException {
+        for (int testNum = 1; testNum <= 15; testNum++) {
+            var pathTest = "go-contest/contest_2023_1/b_problem/tests/";
+            var inputFilePath = Paths.get(pathTest + testNum);
+            var outputFilePath = Paths.get(pathTest + testNum + ".a");
+
+            var result = runTest(inputFilePath);
+            var expectedResult = Files.readString(outputFilePath);
+            var testStatus = result.equals(expectedResult);
+
+            System.out.println(String.format("TEST %d; RESULT: %s", testNum, testStatus));
         }
-        System.out.println(result.toString());
-        sc.close();
+    }
+
+    private static String runTest(Path path) {
+        var sb = new StringBuilder();
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            var inputCount = Integer.valueOf(br.readLine());
+            for (int i = 0; i < inputCount; i++) {
+                var input = br.readLine();
+                var output = isShipCorrect(input);
+                sb.append(output);
+                if (i < inputCount-1) {
+                    sb.append(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+
+        return sb.toString();
     }
 
     public static String isShipCorrect(String str) {
-        Map<String, Integer> shipMap = new HashMap<>();
-        String[] arr = str.split(" ");
-        for (String s : arr) {
-            if (shipMap.containsKey(s)) {
-                shipMap.put(s, shipMap.get(s) + 1);
-            } else {
-                shipMap.put(s, 1);
-            }
+        int[] shipMap = new int[]{0,0,0,0,0};
+
+        var arr = str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            shipMap[Integer.valueOf(arr[i])]++;
         }
 
-        return "YES";
+        var result = shipMap[1] == 4 && shipMap[2] == 3 && shipMap[3] == 2 && shipMap[4] == 1;
+
+        return result ? "YES" : "NO";
     }
 }
