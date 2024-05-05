295ea416ed112746da2f593ee09845e6dafde653
hexhoc <hexhoc@gmail.com>
2024-05-05 23:06:04 +0300

    _26_remove_duplicates_from_sorted_array

--- problems/arrays/deleting/_26_remove_duplicates_from_sorted_array.java
+++ problems/arrays/deleting/_26_remove_duplicates_from_sorted_array.java
@@ -1,49 +1,5 @@
 package problems.arrays.deleting;
 
-/*
-Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
-each unique element appears only once. The relative order of the elements should be kept the same.
-Then return the number of unique elements in nums.
-
-Consider the number of unique elements of nums to be k, to get accepted, you need to do the following
-things:
-
-Change the array nums such that the first k elements of nums contain the unique elements in the order
-they were present in nums initially. The remaining elements of nums are not important as well as the
-size of nums.
-Return k.
-
-Custom Judge:
-
-The judge will test your solution with the following code:
-
-int[] nums = [...]; // Input array
-int[] expectedNums = [...]; // The expected answer with correct length
-
-int k = removeDuplicates(nums); // Calls your implementation
-
-assert k == expectedNums.length;
-for (int i = 0; i < k; i++) {
-    assert nums[i] == expectedNums[i];
-}
-If all assertions pass, then your solution will be accepted.
-
-Example 1:
-
-Input: nums = [1,1,2]
-Output: 2, nums = [1,2,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-
-
-Example 2:
-
-Input: nums = [0,0,1,1,1,2,2,3,3,4]
-Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
-It does not matter what you leave beyond the returned k (hence they are underscores).
-*/
-
 public class _26_remove_duplicates_from_sorted_array {
     public static void main(String[] args) {
         int[] arr1 = new int[]{0,0,1,1,1,2,2,3,3,4};
@@ -52,21 +8,24 @@ public class _26_remove_duplicates_from_sorted_array {
         int[] arr4 = new int[]{0,1,2,2,3,3,4,4,4};
 
         var s = new _26_remove_duplicates_from_sorted_array();
-        s.execute(arr1);
-        s.execute(arr2);
-        s.execute(arr3);
-        s.execute(arr4);
+        var k1 = s.execute(arr1);
+        var k2 = s.execute(arr2);
+        var k3 = s.execute(arr3);
+        var k4 = s.execute(arr4);
+
+        assertEquals(arr1, new int[]{0,1,2,3,4}, k1);
+        assertEquals(arr2, new int[]{1,2}, k2);
+        assertEquals(arr3, new int[]{1}, k3);
+        assertEquals(arr4, new int[]{0,1,2,3,4}, k4);
     }
 
     public int execute(int[] nums) {
         int k = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
-                continue;
+            if (nums[i] != nums[i-1]) {
+                nums[k] = nums[i];
+                k++;
             }
-
-            nums[k] = nums[i];
-            k++;
         }
 
         for (int i = k; i < nums.length; i++) {
@@ -76,4 +35,14 @@ public class _26_remove_duplicates_from_sorted_array {
         return k;
     }
 
+    private static void assertEquals(int[] actual, int[] expected, int actualLength) {
+        if (expected.length != actualLength) {
+            System.out.printf("wrong length. expected: %s, actual: %s%n", expected.length, actualLength);
+        }
+        for (int i = 0; i < actualLength; i++) {
+            if (actual[i] != expected[i]) {
+                System.out.printf("wrong element expected: %s, actual: %s%n", expected[i], actual[i]);
+            }
+        }
+    }
 }
