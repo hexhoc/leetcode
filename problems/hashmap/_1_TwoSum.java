32143fc9978196d387a1cc7070a6621aa5f51e85
hexhoc <Vladislav.ZHURAVSKY@raiffeisen.ru>
2024-11-30 12:16:59 +0300

    update problems 1

--- problems/hashmap/_1_TwoSum.java
+++ problems/hashmap/_1_TwoSum.java
@@ -1,31 +1,30 @@
 package problems.hashmap;
 
-import java.util.Arrays;
+import static util.Assertions.assertEquals;
+
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 1. Two Sum
  */
 public class _1_TwoSum {
     public static void main(String[] args) {
-        int[] arr = new int[] { 3, 2, 4 };
-        var indexes = twoSum(arr, 6);
-        System.out.println(Arrays.toString(indexes));
+        var s = new _1_TwoSum();
+        assertEquals(new int[]{1,2}, s.twoSum(new int[] { 3, 2, 4 }, 6));
     }
 
-    public static int[] twoSum(int[] nums, int target) {
-        var diffs = new HashMap<Integer, Integer>();
+    public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-
+        Map<Integer, Integer> diffMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (diffs.containsKey(nums[i])) {
-                result[0] = diffs.get(nums[i]);
+            if (diffMap.containsKey(nums[i])) {
+                result[0] = diffMap.get(nums[i]);
                 result[1] = i;
+                break;
             }
-
-            diffs.put(target - nums[i], i);
+            diffMap.put(target - nums[i], i);
         }
-
         return result;
     }
 }
